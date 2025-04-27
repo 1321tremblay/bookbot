f8b4145a6830851961c5dd3f10d1f456fc299060
Olivier Tremblay <olivier.tremlay.gh@outlook.com>
2025-04-27 14:21:26 -0400

    l5

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_content = f.read()
+
+    return file_content
+
+
+def count_words(file_path):
+    book_content = get_book_text(file_path)
+    book_letter = book_content.split()
+    counter = 0
+    for char in book_letter:
+        counter += 1
+    return counter
+
+
+def main():
+    # print(get_book_text("./books/frankenstein.txt"))
+    num_words = count_words("./books/frankenstein.txt")
+    print(f"{num_words} words found in the document")
+
+
+main()
